c8c3bdd0501349ee5dbe226e13b76ee0c9730364
Walther Awad <47488509+viagraz0r@users.noreply.github.com>
2024-11-06 10:51:21 -0400

    Se comenta el boton de modo oscuro para ocultar esa funcionalidad

--- src/components/ToggleTheme/ToggleTheme.tsx
+++ src/components/ToggleTheme/ToggleTheme.tsx
@@ -15,20 +15,20 @@ import {
 export function ToggleTheme() {
 	const { setTheme } = useTheme();
 
-	return (
-		<DropdownMenu>
-			<DropdownMenuTrigger asChild>
-				<Button variant="outline" size="icon" className="rounded-full">
-					<Sun className="h-[20px] w-[20px] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-					<Moon className="absolute h-[20px] w-[20px] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-					<span className="sr-only">Toggle theme</span>
-				</Button>
-			</DropdownMenuTrigger>
-			<DropdownMenuContent align="end">
-				<DropdownMenuItem onClick={() => setTheme('light')}>Light</DropdownMenuItem>
-				<DropdownMenuItem onClick={() => setTheme('dark')}>Dark</DropdownMenuItem>
-				<DropdownMenuItem onClick={() => setTheme('system')}>System</DropdownMenuItem>
-			</DropdownMenuContent>
-		</DropdownMenu>
-	);
+	// return (
+	// 	<DropdownMenu>
+	// 		<DropdownMenuTrigger asChild>
+	// 			<Button variant="outline" size="icon" className="rounded-full">
+	// 				<Sun className="h-[20px] w-[20px] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+	// 				<Moon className="absolute h-[20px] w-[20px] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+	// 				<span className="sr-only">Toggle theme</span>
+	// 			</Button>
+	// 		</DropdownMenuTrigger>
+	// 		<DropdownMenuContent align="end">
+	// 			<DropdownMenuItem onClick={() => setTheme('light')}>Light</DropdownMenuItem>
+	// 			<DropdownMenuItem onClick={() => setTheme('dark')}>Dark</DropdownMenuItem>
+	// 			<DropdownMenuItem onClick={() => setTheme('system')}>System</DropdownMenuItem>
+	// 		</DropdownMenuContent>
+	// 	</DropdownMenu>
+	// );
 }
