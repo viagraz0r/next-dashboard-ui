24618e15f0d1c6a14a2bdf0a6f9396f7308c56bf
Walther Awad <47488509+viagraz0r@users.noreply.github.com>
2024-11-06 14:07:46 -0400

    Se agrega hover al menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -7,15 +7,15 @@ const menuItems = [
 		items: [
 			{
 				icon: '/home.png',
-				label: 'Home',
+				label: 'Inicio',
 				href: '/',
 				visible: ['admin', 'patient'],
 			},
 			{
-				icon: '/teacher.png',
-				label: 'Patients',
+				icon: '/student.png',
+				label: 'Pacientes',
 				href: '/list/patients',
-				visible: ['admin', 'teacher'],
+				visible: ['admin', 'patient'],
 			},
 		],
 	},
@@ -55,7 +55,7 @@ const Menu = () => {
 						<Link
 							href={item.href}
 							key={item.label}
-							className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2"
+							className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2 md:px-2 rounded-md hover:bg-mySkyLight"
 						>
 							<Image src={item.icon} alt="" width={20} height={20} />
 							<span className="hidden lg:block">{item.label}</span>
