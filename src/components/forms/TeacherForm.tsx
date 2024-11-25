1e2aada2254e0a05ef65dadc7de203151cc044da
Walther Awad <47488509+viagraz0r@users.noreply.github.com>
2024-11-25 17:47:16 -0400

    teacherForm

--- src/components/forms/TeacherForm.tsx
+++ src/components/forms/TeacherForm.tsx
@@ -4,6 +4,7 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 import InputField from '../InputField';
+import Image from 'next/image';
 
 const schema = z.object({
 	name: z.string().min(3, { message: 'Name is Required' }),
@@ -15,7 +16,8 @@ const schema = z.object({
 	cel: z.string().min(11, { message: 'CellPhone is Required' }),
 	address: z.string().min(1, { message: 'Address is Required' }),
 	birthday: z.string({ message: 'Birthday is Required' }),
-	photo: z.instanceof(File, { message: 'Image is Required' }),
+	img: z.instanceof(File, { message: 'Image is Required' }),
+	sex: z.enum(['male', 'female'], { message: 'Sex is Required' }),
 });
 
 type Inputs = z.infer<typeof schema>;
@@ -103,6 +105,30 @@ const TeacherForm = ({ type, data }: { type: 'create' | 'update'; data?: any }) 
 					register={register}
 					error={errors?.birthday}
 				/>
+			</div>
+			<div className="flex flex-col gap-2 w-full md:w-1/4">
+				<label className="text-xs text-gray-500">Sexo</label>
+				<select
+					className="ring-[1.5px] ring-gray-300 p-2 rounded-md text-sm w-full"
+					{...register('sex')}
+					defaultValue={data?.sex}
+				>
+					<option value="male">Masculino</option>
+					<option value="female">Femenino</option>
+				</select>
+				{errors.sex?.message && (
+					<p className="text-xs text-red-400">{errors.sex.message.toString()}</p>
+				)}
+			</div>
+			<div className="flex flex-col gap-2 w-full md:w-1/4">
+				<label className="text-xs text-gray-500 flex items-center gap-2 cursor-pointer">
+					<Image src="/upload.png" alt="" width={28} height={28} />
+					<span>Upload a photo</span>
+				</label>
+				<input type="file" />
+				{errors.img?.message && (
+					<p className="text-xs text-red-400">{errors.img.message.toString()}</p>
+				)}
 			</div>
 			<button className="bg-blue-400 text-white p-2 rounded-md">
 				{type === 'create' ? 'Create' : 'Update'}
